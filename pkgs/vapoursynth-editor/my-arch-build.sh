adbe96193e1e94bf3c147f660b17a29e65836102
IsoaSFlus <me@isoasflus.com>
2020-07-31 09:08:31 +0800

    update

--- pkgs/vapoursynth-editor/my-arch-build.sh
+++ pkgs/vapoursynth-editor/my-arch-build.sh
@@ -3,4 +3,5 @@ BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
 git clone "https://aur.archlinux.org/vapoursynth-editor.git"
 mv ./vapoursynth-editor/* ./
+rm ./vapoursynth-editor -rf
 makepkg -sf --noconfirm
