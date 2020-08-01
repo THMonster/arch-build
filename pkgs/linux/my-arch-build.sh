6ed4854744a25c8376fed14b46f777ac3ecc0654
IsoaSFlus <me@isoasflus.com>
2020-08-01 10:58:13 +0800

    update

--- pkgs/linux/my-arch-build.sh
+++ pkgs/linux/my-arch-build.sh
@@ -9,19 +9,18 @@ mv PKGBUILD.new PKGBUILD
 cat myconfig >> config
 cat config
 
-pkgver=`cat PKGBUILD | sed -nE 's/^pkgver=([0-9.a-zA-Z]+)/\1/p'`
-pkgver=${pkgver}`cat PKGBUILD | sed -nE 's/^pkgrel=([0-9]+)/\1/p'`
-oldpkgver=`cat ../../pkglist.txt | sed -nE 's/^linux=([0-9.a-zA-Z]+)/\1/p'`
+pkgver="linux"
+pkgver=${pkgver}-`cat PKGBUILD | sed -nE 's/^pkgver=([0-9.a-zA-Z]+)/\1/p'`
+pkgver=${pkgver}-`cat PKGBUILD | sed -nE 's/^pkgrel=([0-9]+)/\1/p'`
+oldpkgver=`curl https://api.github.com/repos/isoasflus/arch-build/releases -s | jq '.[0].assets' | grep '"name"' | sed -nE 's/^.+"name": "([^"]+)",$/\1/p' | grep -e 'linux-[0-9]'`
 
 if [[ $oldpkgver == "" ]]
 then
-    echo "linux=0" >> ../../pkglist.txt
+    oldpkgver='a'
 fi
 
 echo ${pkgver} ${oldpkgver}
-if [[ $pkgver != $oldpkgver ]]
+if [[ `vercmp ${pkgver} ${oldpkgver}` == 1  ]]
 then
-    # makepkg -sf --noconfirm --skippgpcheck && sed -i -E 's/linux=[0-9.a-zA-Z]+/linux='${pkgver}'/g' ../../pkglist.txt
-    sed -i -E 's/^linux=[0-9.a-zA-Z]+/linux='${pkgver}'/g' ../../pkglist.txt
+    makepkg -sf --noconfirm --skippgpcheck
 fi
-cat ../../pkglist.txt
