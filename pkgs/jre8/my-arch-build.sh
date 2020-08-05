2f50c9e4967d687afd7bf4afbfb2b09b6cf29f37
IsoaSFlus <me@isoasflus.com>
2020-08-05 08:56:44 +0800

    update

---
+++ pkgs/jre8/my-arch-build.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+BASEDIR=$(dirname "$0")
+cd "$BASEDIR"
+yay -G jre8
+mv ./jre8/* ./
+rm ./jre8 -rf
+
+pkgver="jre8"
+pkgver=${pkgver}-`cat PKGBUILD | sed -nE 's/^pkgver=([0-9.a-zA-Z]+)/\1/p'`
+pkgver=${pkgver}-`cat PKGBUILD | sed -nE 's/^pkgrel=([0-9]+)/\1/p'`
+oldpkgver=`curl https://api.github.com/repos/isoasflus/arch-build/releases -s | jq '.[0].assets' | grep '"name"' | sed -nE 's/^.+"name": "([^"]+)",$/\1/p' | grep -e 'jre8-[0-9]' | sed -n '$p'`
+
+if [[ $oldpkgver == "" ]]
+then
+    oldpkgver='a'
+fi
+
+echo ${pkgver} ${oldpkgver}
+if [[ `vercmp ${pkgver} ${oldpkgver}` == 1  ]]
+then
+    makepkg -sf --noconfirm --skippgpcheck
+fi
