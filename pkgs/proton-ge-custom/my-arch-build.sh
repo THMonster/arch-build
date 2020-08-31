7fc99940c8fac0908c5adbb98ec25fe02da7b5c8
IsoaSFlus <me@isoasflus.com>
2020-08-31 09:12:37 +0800

    update

--- pkgs/proton-ge-custom/my-arch-build.sh
+++ pkgs/proton-ge-custom/my-arch-build.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
-yay -G proton-ge-custom
-mv ./proton-ge-custom/* ./
-rm ./proton-ge-custom -rf
+yay -G proton-ge-custom-bin
+mv ./proton-ge-custom-bin/* ./
+rm ./proton-ge-custom-bin -rf
 
-pkgver="proton-ge-custom-5.11.GE.2.MF-1"
-#pkgver=${pkgver}-`cat PKGBUILD | sed -nE 's/^pkgver=([0-9.a-zA-Z]+)/\1/p'`
-#pkgver=${pkgver}-`cat PKGBUILD | sed -nE 's/^pkgrel=([0-9]+)/\1/p'`
-oldpkgver=`curl https://api.github.com/repos/isoasflus/arch-build/releases -s | jq '.[0].assets' | grep '"name"' | sed -nE 's/^.+"name": "([^"]+)",$/\1/p' | grep -e 'proton-ge-custom-[0-9]' | sed -n '$p'`
+pkgver="proton-ge-custom-bin"
+pkgver=${pkgver}-`cat PKGBUILD | sed -nE 's/^pkgver=([0-9._a-zA-Z]+)/\1/p'`
+pkgver=${pkgver}-`cat PKGBUILD | sed -nE 's/^pkgrel=([0-9]+)/\1/p'`
+oldpkgver=`curl https://api.github.com/repos/isoasflus/arch-build/releases -s | jq '.[0].assets' | grep '"name"' | sed -nE 's/^.+"name": "([^"]+)",$/\1/p' | grep -e 'proton-ge-custom-bin-[0-9]' | sed -n '$p'`
 
 if [[ $oldpkgver == "" ]]
 then
