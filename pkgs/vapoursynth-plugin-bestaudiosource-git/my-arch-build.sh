5d1802505cb4b12fa13027bfd5c3fb3d612aea52
IsoaSFlus <me@isoasflus.com>
2021-10-02 10:16:50 +0800

    update

---
+++ pkgs/vapoursynth-plugin-bestaudiosource-git/my-arch-build.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+PKGNAME="vapoursynth-plugin-bestaudiosource-git"
+BASEDIR=$(dirname "$0")
+cd "$BASEDIR"
+
+pkgver=${PKGNAME}-`cat PKGBUILD | sed -nE 's/^pkgver=([0-9.a-zA-Z]+)/\1/p'`
+pkgver=${pkgver}-`cat PKGBUILD | sed -nE 's/^pkgrel=([0-9]+)/\1/p'`
+oldpkgver=`curl https://api.github.com/repos/isoasflus/arch-build/releases -s | jq '.[0].assets' | grep '"name"' | sed -nE 's/^.+"name": "([^"]+)",$/\1/p' | grep -e "${PKGNAME}-[0-9a-zA-Z]" | sed -n '$p'`
+
+if [[ $oldpkgver == "" ]]
+then
+    oldpkgver='a'
+fi
+
+echo ${pkgver} ${oldpkgver}
+makepkg -sf --noconfirm --skippgpcheck --skipchecksums
