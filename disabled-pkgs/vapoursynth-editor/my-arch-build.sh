7cb43cae890f0435751dd7dbaf2530ac06f01587
IsoaSFlus <me@isoasflus.com>
2021-10-02 16:17:23 +0800

    update

--- disabled-pkgs/vapoursynth-editor/my-arch-build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-BASEDIR=$(dirname "$0")
-cd "$BASEDIR"
-git clone "https://aur.archlinux.org/vapoursynth-editor.git"
-mv ./vapoursynth-editor/* ./
-rm ./vapoursynth-editor -rf
-rm .git -rf
-makepkg -sf --noconfirm
