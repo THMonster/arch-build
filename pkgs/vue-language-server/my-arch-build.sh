2a7dfed7dab0f61ace9ba50775608f5363662b72
THMonster <me@isoasflus.com>
2022-05-23 15:07:29 +0800

    update

--- pkgs/vue-language-server/my-arch-build.sh
+++ pkgs/vue-language-server/my-arch-build.sh
@@ -1,15 +1,25 @@
 #! /bin/sh
-PKGNAME="nodejs-vls-git"
+PKGNAME="vue-language-server"
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
+
+paru -G ${PKGNAME}
+mv ./${PKGNAME}/* ./
+rm ./${PKGNAME} -rf
 
 #git clone https://git.archlinux.org/svntogit/packages.git --single-branch -b packages/${PKGNAME}
 #mv ./packages/repos/extra-x86_64/* ./
 #rm ./packages -rf
 
+
 pkgver=${PKGNAME}-`cat PKGBUILD | sed -nE 's/^pkgver=([0-9.a-zA-Z]+)/\1/p'`
 pkgver=${pkgver}-`cat PKGBUILD | sed -nE 's/^pkgrel=([0-9]+)/\1/p'`
 oldpkgver=`curl https://api.github.com/repos/THMonster/arch-build/releases -s | jq '.[0].assets' | grep '"name"' | sed -nE 's/^.+"name": "([^"]+)",$/\1/p' | grep -e "${PKGNAME}-[0-9a-zA-Z]" | sed -n '$p'`
+
+# git clone -b <br> <url> upstream-git --depth=1
+# cd upstream-git
+# pkgver=`git describe --always --dirty`
+# cd ..
 
 if [[ $oldpkgver == "" ]]
 then
@@ -17,4 +27,8 @@ then
 fi
 
 echo ${pkgver} ${oldpkgver}
-makepkg -sf --noconfirm --skippgpcheck --skipchecksums
+if [[ `vercmp ${pkgver}-aaa ${oldpkgver}` == 1  ]]
+# if [[ `echo ${oldpkgver} | grep ${pkgver}` == ""  ]]
+then
+    makepkg -sf --noconfirm --skippgpcheck --skipchecksums
+fi
