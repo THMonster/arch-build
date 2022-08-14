b05513bf56cb17bfd95e3dd53bf760444b3f88ee
THMonster <me@isoasflus.com>
2022-08-14 18:54:09 +0800

    1

--- pkgs/dmlive-git/my-arch-build.sh
+++ pkgs/dmlive-git/my-arch-build.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-PKGNAME="revda-git"
+PKGNAME="dmlive-git"
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
 
@@ -15,7 +15,7 @@ pkgver=${PKGNAME}-`cat PKGBUILD | sed -nE 's/^pkgver=([0-9.a-zA-Z]+)/\1/p'`
 pkgver=${pkgver}-`cat PKGBUILD | sed -nE 's/^pkgrel=([0-9]+)/\1/p'`
 oldpkgver=`curl https://api.github.com/repos/THMonster/arch-build/releases -s | jq '.[0].assets' | grep '"name"' | sed -nE 's/^.+"name": "([^"]+)",$/\1/p' | grep -e "${PKGNAME}-[0-9a-zA-Z]" | sed -n '$p'`
 
-git clone -b developing https://github.com/THMonster/Revda.git upstream-git --depth=1
+git clone -b developing https://github.com/THMonster/dmlive.git upstream-git --depth=1
 cd upstream-git
 pkgver=`git describe --always --dirty`
 cd ..
