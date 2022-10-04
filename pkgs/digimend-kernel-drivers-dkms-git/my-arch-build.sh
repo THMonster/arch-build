fe20ec8a35fe9b54d272919727dd2ff537264795
THMonster <me@isoasflus.com>
2022-10-04 13:40:29 +0800

    1

--- pkgs/digimend-kernel-drivers-dkms-git/my-arch-build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-BASEDIR=$(dirname "$0")
-cd "$BASEDIR"
-yay -G digimend-kernel-drivers-dkms-git
-mv ./digimend-kernel-drivers-dkms-git/* ./
-rm ./digimend-kernel-drivers-dkms-git -rf
-
-makepkg -sf --noconfirm --skippgpcheck
--- pkgs/vapoursynth-editor-git/my-arch-build.sh
+++ pkgs/vapoursynth-editor-git/my-arch-build.sh
@@ -3,7 +3,7 @@ PKGNAME="vapoursynth-editor-sandsmark-git"
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
 
-yay -G ${PKGNAME}
+paru -G ${PKGNAME}
 mv ./${PKGNAME}/* ./
 rm ./${PKGNAME} -rf
 
@@ -15,10 +15,18 @@ pkgver=${PKGNAME}-`cat PKGBUILD | sed -nE 's/^pkgver=([0-9.a-zA-Z]+)/\1/p'`
 pkgver=${pkgver}-`cat PKGBUILD | sed -nE 's/^pkgrel=([0-9]+)/\1/p'`
 oldpkgver=`curl https://api.github.com/repos/THMonster/arch-build/releases -s | jq '.[0].assets' | grep '"name"' | sed -nE 's/^.+"name": "([^"]+)",$/\1/p' | grep -e "${PKGNAME}-[0-9a-zA-Z]" | sed -n '$p'`
 
+git clone -b master https://github.com/sandsmark/vapoursynth-editor upstream-git --depth=1
+cd upstream-git
+pkgver=`git describe --always --dirty`
+cd ..
+
 if [[ $oldpkgver == "" ]]
 then
     oldpkgver='a'
 fi
 
 echo ${pkgver} ${oldpkgver}
-makepkg -sf --noconfirm  --skippgpcheck --skipchecksums
+if [[ `echo ${oldpkgver} | grep ${pkgver}` == ""  ]]
+then
+    makepkg -sf --noconfirm --skippgpcheck --skipchecksums
+fi
