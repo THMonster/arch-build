438da624538ac17eb01413535c2bbe5c68276b1b
THMonster <me@isoasflus.com>
2022-10-30 09:09:00 +0800

    1

---
+++ pkgs/flyctl-bin/my-arch-build.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+PKGNAME="flyctl-bin"
+BASEDIR=$(dirname "$0")
+cd "$BASEDIR"
+
+paru -G ${PKGNAME}
+mv ./${PKGNAME}/* ./
+rm ./${PKGNAME} -rf
+
+#git clone https://git.archlinux.org/svntogit/packages.git --single-branch -b packages/${PKGNAME}
+#mv ./packages/repos/extra-x86_64/* ./
+#rm ./packages -rf
+
+
+pkgver=${PKGNAME}-`cat PKGBUILD | sed -nE 's/^pkgver=([0-9.a-zA-Z]+)/\1/p'`
+pkgver=${pkgver}-`cat PKGBUILD | sed -nE 's/^pkgrel=([0-9]+)/\1/p'`
+oldpkgver=`curl https://api.github.com/repos/THMonster/arch-build/releases -s | jq '.[0].assets' | grep '"name"' | sed -nE 's/^.+"name": "([^"]+)",$/\1/p' | grep -e "${PKGNAME}-[0-9a-zA-Z]" | sed -n '$p'`
+
+# git clone -b <br> <url> upstream-git --depth=1
+# cd upstream-git
+# pkgver=`git describe --always --dirty`
+# cd ..
+
+if [[ $oldpkgver == "" ]]
+then
+    oldpkgver='a'
+fi
+
+echo ${pkgver} ${oldpkgver}
+if [[ `vercmp ${pkgver}-aaa ${oldpkgver}` == 1  ]]
+# if [[ `echo ${oldpkgver} | grep ${pkgver}` == ""  ]]
+then
+    makepkg -sf --noconfirm --skippgpcheck --skipchecksums
+fi
