ef6d740a83145cd7957c661fcd3b23d1ef94e096
THMonster <me@isoasflus.com>
2023-05-02 11:57:40 +0800

    update

--- pkgs/gamescope-git/my-arch-build.sh
+++ pkgs/gamescope-git/my-arch-build.sh
@@ -16,10 +16,10 @@ pkgver=${PKGNAME}-`cat PKGBUILD | sed -nE 's/^pkgver=([0-9.a-zA-Z]+)/\1/p'`
 pkgver=${pkgver}-`cat PKGBUILD | sed -nE 's/^pkgrel=([0-9]+)/\1/p'`
 oldpkgver=`curl https://api.github.com/repos/THMonster/arch-build/releases -s | jq '.[0].assets' | grep '"name"' | sed -nE 's/^.+"name": "([^"]+)",$/\1/p' | grep -e "${PKGNAME}-[0-9a-zA-Z]" | sed -n '$p'`
 
-# git clone -b <br> <url> upstream-git --depth=1
-# cd upstream-git
-# pkgver=`git describe --always --dirty`
-# cd ..
+git clone -b master https://github.com/ValveSoftware/gamescope upstream-git --depth=1
+cd upstream-git
+pkgver=`git describe --always --dirty`
+cd ..
 
 if [[ $oldpkgver == "" ]]
 then
@@ -27,8 +27,8 @@ then
 fi
 
 echo ${pkgver} ${oldpkgver}
-if [[ `vercmp ${pkgver}-aaa ${oldpkgver}` == 1  ]]
-# if [[ `echo ${oldpkgver} | grep ${pkgver}` == ""  ]]
+# if [[ `vercmp ${pkgver}-aaa ${oldpkgver}` == 1  ]]
+if [[ `echo ${oldpkgver} | grep ${pkgver}` == ""  ]]
 then
     makepkg -sf --noconfirm --skippgpcheck --skipchecksums
 fi
