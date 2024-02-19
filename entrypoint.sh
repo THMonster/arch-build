93b8153b1b4042b2c818eb889f317cc06f23dd89
THMonster <me@isoasflus.com>
2024-02-19 14:30:58 +0800

    1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env nu
 
-ls
-cd pkgs
-chown packer ./ -R
-ls | xargs -i sudo -u packer sh -c 'sh {}/my-arch-build.sh; mv {}/*.pkg.tar.zst ./; rm {} -rf'
+def main [] {
+    ls
+    cd pkgs
+    chown packer ./ -R
+    # ls | xargs -i sudo -u packer sh -c 'sh {}/my-arch-build.sh; mv {}/*.pkg.tar.zst ./; rm {} -rf'
+    ls | each { |it|
+		try { sh $"($it.name)/my-arch-build.sh" }
+		try { rm $"($it.name)-debug*.pkg.tar.zst" }
+		try { mv "*.pkg.tar.zst" ./ }
+		try { rm $it.name -rf }
+	}
+}
+
+
+
+
