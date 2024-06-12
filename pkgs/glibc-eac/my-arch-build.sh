4367110a68199797bcce3c643f606b455815a0ed
THMonster <me@isoasflus.com>
2024-06-12 22:24:35 +0800

    1

--- pkgs/glibc-eac/my-arch-build.sh
+++ pkgs/glibc-eac/my-arch-build.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-PKGNAME="gamescope-git"
+PKGNAME="glibc-eac"
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
 
@@ -16,10 +16,10 @@ pkgver=${PKGNAME}-`cat PKGBUILD | sed -nE 's/^pkgver=([0-9.a-zA-Z]+)/\1/p'`
 pkgver=${pkgver}-`cat PKGBUILD | sed -nE 's/^pkgrel=([0-9]+)/\1/p'`
 oldpkgver=`curl https://api.github.com/repos/THMonster/arch-build/releases -s | jq '.[0].assets' | grep '"name"' | sed -nE 's/^.+"name": "([^"]+)",$/\1/p' | grep -e "${PKGNAME}-[0-9a-zA-Z]" | sed -n '$p'`
 
-git clone -b master https://github.com/ValveSoftware/gamescope upstream-git --depth=1
-cd upstream-git
-pkgver=`git describe --always --dirty`
-cd ..
+# git clone -b <br> <url> upstream-git --depth=1
+# cd upstream-git
+# pkgver=`git describe --always --dirty`
+# cd ..
 
 if [[ $oldpkgver == "" ]]
 then
@@ -27,8 +27,8 @@ then
 fi
 
 echo ${pkgver} ${oldpkgver}
-# if [[ `vercmp ${pkgver}-aaa ${oldpkgver}` == 1  ]]
-if [[ `echo ${oldpkgver} | grep ${pkgver}` == ""  ]]
+if [[ `vercmp ${pkgver}-aaa ${oldpkgver}` == 1  ]]
+# if [[ `echo ${oldpkgver} | grep ${pkgver}` == ""  ]]
 then
 	paru -B --skipreview --noconfirm .
 fi
