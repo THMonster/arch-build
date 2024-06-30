246dd258403b9d566dd90ca7e2b04430959c713a
THMonster <me@isoasflus.com>
2024-06-30 09:51:52 +0800

    1

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,11 @@ def main [] {
     ls
     cd pkgs
     chown packer ./ -R
+    let package_list = (curl https://api.github.com/repos/THMonster/arch-build/releases -s | get 0.assets | sort-by created_at | get name | to text)
     # ls | xargs -i sudo -u packer sh -c 'sh {}/my-arch-build.sh; mv {}/*.pkg.tar.zst ./; rm {} -rf'
     ls | each { |it|
-		try { 
-			sudo -u packer sh $"($it.name)/my-arch-build.sh" 
+		try {
+			sudo -u packer sh $"($it.name)/my-arch-build.sh" ${package_list}
         }
 		let pkg_d = (glob $"*/($it.name)-debug*.pkg.tar.zst")
 		try { $pkg_d | get 0 | rm $in }
@@ -17,7 +18,3 @@ def main [] {
 		try { rm $it.name -rf }
 	}
 }
-
-
-
-
