48f43bbb26903aa1fc90a1b3d34c7df04462682d
THMonster <me@isoasflus.com>
2024-06-30 09:56:29 +0800

    1

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ def main [] {
     # ls | xargs -i sudo -u packer sh -c 'sh {}/my-arch-build.sh; mv {}/*.pkg.tar.zst ./; rm {} -rf'
     ls | each { |it|
 		try {
-			sudo -u packer sh $"($it.name)/my-arch-build.sh" ${package_list}
+			sudo -u packer sh $"($it.name)/my-arch-build.sh" $package_list
         }
 		let pkg_d = (glob $"*/($it.name)-debug*.pkg.tar.zst")
 		try { $pkg_d | get 0 | rm $in }
