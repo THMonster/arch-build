554a006d61f8ab451bfb6f5d1e0534d971e40b4e
THMonster <me@isoasflus.com>
2024-07-01 11:00:41 +0800

    1

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,15 +8,19 @@ def main [] {
         touch /tmp/packages.txt
         curl https://api.github.com/repos/THMonster/arch-build/releases -s | from json | get 0.assets | sort-by created_at | get name | save -f /tmp/packages.txt
     }
-    # ls | xargs -i sudo -u packer sh -c 'sh {}/my-arch-build.sh; mv {}/*.pkg.tar.zst ./; rm {} -rf'
+    cat /etc/makepkg.conf | str replace -m -r '(^OPTIONS=\([^)]*)debug([^)]*\))' '${1}!debug$2' | save -f /tmp/makepkg.conf
+    mv /tmp/makepkg.conf /etc/makepkg.conf
     ls | each { |it|
 		try {
 			sudo -u packer sh $"($it.name)/my-arch-build.sh"
         }
-		let pkg_d = (glob $"*/($it.name)-debug*.pkg.tar.zst")
-		try { $pkg_d | get 0 | rm $in }
+		# let pkg_d = (glob $"*/($it.name)-debug*.pkg.tar.zst")
+		# try { $pkg_d | get 0 | rm $in }
 		let pkg = (glob $"($it.name)/*.pkg.tar.zst")
-		try { $pkg | get 0 | mv $in ./ }
+		$pkg | each { |p|
+    		try { mv $p ./ }
+		}
+		# try { $pkg | get 0 | mv $in ./ }
 		tree -L 2
 		try { rm $it.name -rf }
 	}
